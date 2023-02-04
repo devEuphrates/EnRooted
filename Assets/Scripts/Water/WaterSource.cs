c661e665b15f99f392ba96488f07153ef1f42316
egeserbes <egeserbes55@gmail.com>
2023-02-04 20:15:10 +0300

    water source code

--- Assets/Scripts/Water/WaterSource.cs
+++ Assets/Scripts/Water/WaterSource.cs
@@ -10,6 +10,7 @@ public class WaterSource : MonoBehaviour
     [SerializeField] private float _decrease_by_Value;
     [SerializeField] private float _approach_rate;
     Collider _collider;
+    bool _enabled = true;
     private void Awake()
     {
         _currentValue = _baseValue;
@@ -18,15 +19,34 @@ public class WaterSource : MonoBehaviour
     }
     public void ModifyValue()
     {
-        _targetValue = (_targetValue - _decrease_by_Value <= 0) ? 0 : _targetValue - _decrease_by_Value;
-        Debug.Log(_targetValue);
-        if (_targetValue == 0) _collider.isTrigger = false;
+        if (_enabled)
+        {
+            _targetValue = ((_targetValue - _decrease_by_Value) <= 0) ? 0 : _targetValue - _decrease_by_Value;
+            Debug.Log(_targetValue);
+
+        }
+        if (_targetValue == 0)
+        {
+            _collider.enabled = false;
+            _enabled = false;
+        }
     }
     void Update()
     {
+
         var temp = transform.localScale;
         _currentValue = Mathf.Lerp(_currentValue, _targetValue, _approach_rate);
+        if (transform.localScale.y <= 0.01)
+        {
+            Debug.Log("dasdasd");
+            gameObject.SetActive(false);
+        }
+
         temp.y = _currentValue / _baseValue;
         transform.localScale = temp;
+
+    
+
+
     }
 }
